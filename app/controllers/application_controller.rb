254e595974a7d793d680802b91a81b903e5e6266
Broc Nickodemus <nickodeb@oregonstate.edu>
2017-11-07 12:03:42 -0800

    added sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in) do |user_params|
       user_params.permit(:username, :email)
