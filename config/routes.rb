175544d8edfa2c6b714ea3c69a19f0b0403f133f
Broc Nickodemus <nickodeb@oregonstate.edu>
2017-11-09 11:02:56 -0800

    got login working

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  #devise_for :users, controllers: { sessions: 'users/sessions' }
-  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
 
-  resources :event
   root to: 'home#index'
   get 'home/index'
+  get 'user_root' => 'events#index', as: :user_root
 
-  #get '/auth/login', to: 'devise/session#new', as: 'user_root_path'
-  #get 'user_root' => 'devise/session#new', as: :user_root
-
+  resources :events
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
