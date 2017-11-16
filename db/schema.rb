4669310f590c01699ecb4f4b3c2ba4a9751b086d
gonzalky7 <gonzalky@oregonstate.edu>
2017-11-16 11:10:54 -0800

    Pull

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(version: 20171114112641) do
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.datetime "date"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
